65997fdb7f3a76ba2eb9823023571220908895b0
Yura Park <yrpr1014@gamil.com>
2022-03-07 14:20:11 +0900

    merge

--- src/javafirst/Hello.java
+++ src/javafirst/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("part1");
+		System.out.println("branch1");
+		System.out.println("Hello");		
+		System.out.println("one");		
+
 		// TODO Auto-generated method stub
 
 	}
